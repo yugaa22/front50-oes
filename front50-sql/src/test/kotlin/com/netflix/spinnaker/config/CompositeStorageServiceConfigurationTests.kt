318891b10455071e3ecc189afb5587084426cb0f
SheetalAtre <sheetal.atre@opsmx.io>
2023-04-28 22:27:28 +0530

    changes to update spring-core version 6.0.2 to 6.0.8

--- front50-sql/src/test/kotlin/com/netflix/spinnaker/config/CompositeStorageServiceConfigurationTests.kt
+++ front50-sql/src/test/kotlin/com/netflix/spinnaker/config/CompositeStorageServiceConfigurationTests.kt
@@ -16,8 +16,7 @@
 
 package com.netflix.spinnaker.config
 
-import com.netflix.spectator.api.DefaultRegistry
-import com.netflix.spectator.api.Registry
+
 import com.netflix.spinnaker.front50.config.CommonStorageServiceDAOConfig
 import com.netflix.spinnaker.front50.config.StorageServiceConfigurationProperties
 import com.netflix.spinnaker.front50.migrations.StorageServiceMigrator
@@ -67,15 +66,10 @@ internal class CompositeStorageServiceConfigurationTests {
 }
 
 @SpringBootApplication
-@Import(CommonStorageServiceDAOConfig::class, CompositeStorageServiceConfiguration::class, SqlConfiguration::class, PluginsAutoConfiguration::class)
+@Import(CommonStorageServiceDAOConfig::class, CompositeStorageServiceConfiguration::class, SqlConfiguration::class)
 @EnableConfigurationProperties(StorageServiceConfigurationProperties::class)
 internal class CompositeStorageServiceConfigurationTestApp {
   @Bean
   @ConditionalOnMissingBean
   fun contextProvider(): RequestContextProvider = AuthenticatedRequestContextProvider()
-
-  @Bean
-  @ConditionalOnMissingBean
-  fun getRegistry(): Registry = DefaultRegistry()
-
 }
